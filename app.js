c4dbc418579168bb8e721dff232f66a81bfcfaa1
jgelsey <github@gelsey.com>
2020-04-11 14:07:27 -0700

    Upgrade to auth0.js V9

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ var strategy = new Auth0Strategy({
     domain: process.env.AUTH0_DOMAIN,
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK
+    callbackURL: process.env.AUTH0_CALLBACK,
+    redirectUri: process.env.AUTH0_CALLBACK,    // <!-- upgrade to Lock 9 -->
+    scope: "openid email"                       // <!-- upgrade to Lock 9 -->
 }, function(accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
     // extraParams.id_token has the JSON Web Token
